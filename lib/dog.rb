4f8dae93016a80f735ce7536239ce6cefac5b30b
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-09 18:28:16 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def initialize(name, breed='Mut')
+    @name = name
+    @breed =breed
+  end
+
+
+  def breed=(breed)
+    @breed = breed
+
+  end
+
+  def breed
+    @breed
+  end
+
+end
